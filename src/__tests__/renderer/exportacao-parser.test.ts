48004f49a55604c14d02a6511d1c9a718493d198
issjunior <issjunior@live.com>
2026-08-23 18:19:25 -0300

    ajuste_exportacao_docx_odt_beta

--- src/__tests__/renderer/exportacao-parser.test.ts
+++ src/__tests__/renderer/exportacao-parser.test.ts
@@ -7,7 +7,7 @@ describe('parseHtmlParaEstrutura', () => {
       <h2>Seção</h2><p style="text-align:justify;line-height:1.5;margin-left:12pt">
       <strong>Negrito <em style="color:#123456;background-color:#ffee00">e itálico</em></strong><sup>2</sup>&nbsp;<a href="https://exemplo.test">link</a></p>
       <ol><li>Primeiro<ul><li>Filho</li></ul></li></ol>
-      <table><tr><th colspan="2" style="background-color:#eeeeee">Cabeçalho</th></tr><tr><td rowspan="2">A</td><td>B</td></tr></table>
+      <table><tr><th colspan="2" style="background-color:#eeeeee">Cabeçalho</th></tr><tr><td rowspan="2" style="background-color:#000000">A</td><td>B</td></tr></table>
       <blockquote>citação</blockquote><pre>  código</pre><hr>
       <figure style="text-align:center"><img width="320" height="180" src="data:image/png;base64,aGVsbG8="><figcaption>Figura 1: teste</figcaption></figure>`);
 
@@ -24,7 +24,10 @@ describe('parseHtmlParaEstrutura', () => {
     expect(blocos.filter(b => b.tipo === 'lista')).toHaveLength(2);
     const tabela = blocos.find(b => b.tipo === 'tabela');
     expect(tabela).toMatchObject({ tipo: 'tabela' });
-    if (tabela?.tipo === 'tabela') expect(tabela.linhas[0][0]).toMatchObject({ colspan: 2, corFundo: 'EEEEEE' });
+    if (tabela?.tipo === 'tabela') {
+      expect(tabela.linhas[0][0]).toMatchObject({ colspan: 2, corFundo: 'EEEEEE' });
+      expect(tabela.linhas[1][0].corFundo).toBeUndefined();
+    }
     expect(blocos).toEqual(expect.arrayContaining([expect.objectContaining({ tipo: 'linha-horizontal' }), expect.objectContaining({ tipo: 'figura', larguraPx: 320, alturaPx: 180 })]));
   });
 });
